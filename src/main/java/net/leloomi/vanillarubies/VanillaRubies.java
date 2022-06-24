e3f3ba8354bfbc5280ce32b72a5cdb3dface9850
LeLoomi <lohr.eliah@gmail.com>
2022-06-24 23:02:33 +0200

    Initial commit

---
+++ src/main/java/net/leloomi/vanillarubies/VanillaRubies.java
@@ -0,0 +1,15 @@
+package net.leloomi.vanillarubies;
+
+import net.fabricmc.api.ModInitializer;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+public class VanillaRubies implements ModInitializer {
+	public static final String MOD_ID = "vanillarubies";
+	public static final Logger LOGGER = LoggerFactory.getLogger(MOD_ID);
+
+	@Override
+	public void onInitialize() {
+
+	}
+}
---
+++ src/main/java/net/leloomi/vanillarubies/VanillaRubiesClient.java
@@ -0,0 +1,12 @@
+package net.leloomi.vanillarubies;
+
+import net.fabricmc.api.ClientModInitializer;
+
+public class VanillaRubiesClient implements ClientModInitializer
+{
+    @Override
+    public void onInitializeClient()
+    {
+
+    }
+}
